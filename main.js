9825854447b54909477b1c5de97d34cb8e5a97e1
themodgamer <roveralexander2014@gmail.com>
2022-05-16 11:44:10 +0200

    Completed the transfer from one alarm per day to infinite amount of alarms for any day also did some random improvements got a 100 score on lighthouse

--- main.js
+++ main.js
@@ -15,33 +15,33 @@ app.use((req, res, next) => {
     req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
 })
 
+app.use((req, res, next) => {
+    commoncode.Log(commoncode.iptranslate(req.ip.toString()),req.method +  " " + req.path);
+    res.cookie('ALL', '_ALL_COOKIES', { sameSite: 'none', secure: true})
+    next()
+  })
+
 //renders all of the pages that must be rendered
 commoncode.foerachfile('./renderpages',renderpages)
 function renderpages(filename) {
-    try {
+    commoncode.fuse(function () {
         var json = new jsonfunctions.Jsonedit(filename)
         app.get(json.JSON.address, function (req, res) {
-            commoncode.Log(commoncode.iptranslate(req.ip.toString()),"GET " + json.JSON.name);
-            res.cookie('ALL', '_ALL_COOKIES', { sameSite: 'none', secure: true})
             res.render(json.JSON.filename, updateinputs(req,res) )
         })
-    } catch(e) {
-        console.warn(e.message);
-    }
+    });
 }
 
 //shares all files that it is supposed to share
 commoncode.foerachfile('./sharedfilles',sharefile)
 function sharefile(filename) {
-    try {
+    commoncode.fuse(function () {
         var json = new jsonfunctions.Jsonedit(filename)
         app.get(json.JSON.address, function (req, res) {
-            commoncode.Log(commoncode.iptranslate(req.ip.toString()),"GET " + json.JSON.name);
+            res.set("Content-Security-Policy", "default-src 'self'");
             res.sendFile(path.join(__dirname, json.JSON.filename));
         })
-    } catch(e) {
-        console.warn(e.message);
-    }
+    });
 }
 
 function refreshweather() {
@@ -49,7 +49,7 @@ function refreshweather() {
     var jsonfile = new jsonfunctions.Jsonedit("./inputs/Weather.json")
     for (var index = 0; index < 2; index++) {
         var item = jsonfile.JSON[index]
-        try {
+        commoncode.fuse(function () {
             https.get('https://api.weatherapi.com/v1/forecast.json?key=8c048176fc0d405e80192210221403&alerts=no&days=3&q=' + item.location,res => {
             let data = [];
             res.on('data', chunk => {
@@ -64,21 +64,63 @@ function refreshweather() {
                 item.toptext[1] = weatherdata.forecast.forecastday[1].day.condition.text
                 item.toptext[2] = weatherdata.forecast.forecastday[2].day.condition.text
             })})
-        } catch (error) {
-            console.log(error)
-        }
+        });
     }
     jsonfile.applychanges()
 }
-
 refreshweather()
+
 function updateinputs(req, res) {
     var Common = require('./inputs/Common.json');
     var UserData = require('./inputs/UserData.json');
-    
-    //find one with the closets hour
 
-    var selectedindex 
+    var currentdate = new Date();
+    var currentday = currentdate.getDay()-1;
+    var currenthour = currentdate.getHours();
+    var currentminutes = currentdate.getMinutes()
+
+    var alarmstoday = [];
+    Common.subclocktime = "No Alarms Today";
+
+    UserData.Alarms.forEach(element => {
+        if (parseInt(element.Day) == currentday) {
+            alarmstoday.push({"Day": element.Day, "Time":element.Time})
+        }
+    });
+    
+
+    var closesthour = 90;
+    var closestminutes = 90;
+    if (alarmstoday.length > 0) {
+        alarmstoday.forEach(element => {
+            var splitobject = String(element.Time).split(":");
+            var hour = parseInt(splitobject[0]);
+            var minute = parseInt(splitobject[1]);
+
+            //if hour is larger or same as current time
+            if (closestminutes < currentminutes)
+                closestminutes = 90;
+            if (hour > currenthour || hour == currenthour) {
+                
+                if (hour == closesthour) {
+                    console.log(minute < closestminutes,minute,closestminutes)
+                    if (minute >= currentminutes && minute < closestminutes) {
+                        closestminutes = minute;
+                        Common.subclocktime = "Next Alarm " + commoncode.addzero(closesthour) + ":" + commoncode.addzero(closestminutes) + ".";
+                    }
+                }
+                if (hour < closesthour) {
+                    closesthour = hour;
+                    closestminutes = minute;
+                    Common.subclocktime = "Next Alarm " + commoncode.addzero(closesthour) + ":" + commoncode.addzero(closestminutes) + ".";
+                }
+            }
+            
+        });
+
+        
+    }
+
     //set variables
     Common.clientip = commoncode.iptranslate(req.ip.toString())
     //render everything on server and send to client
@@ -88,10 +130,10 @@ function updateinputs(req, res) {
 }
 
 app.post('/settingsupdate', urlencodedParser, function (req, res) {
-    res.set("Content-Security-Policy", "default-src 'self'");
-    commoncode.Log(req.ip.toString(),"POST /settingsupdate");
-    console.log(req.body)
     var jsonfile = new jsonfunctions.Jsonedit('./inputs/UserData.json')
+    if (req.body.newalarmdaynum != null && req.body.newalarmtime != null && req.body.newalarmtime != "NaN" && req.body.newalarmdaynum != "Nan" && req.body.newalarmtime != "" && req.body.newalarmdaynum != "") {
+        jsonfile.JSON.Alarms.push({"Day":Math.max(Math.min(parseInt(req.body.newalarmdaynum),6),0).toString(),"Time": req.body.newalarmtime });
+    }
     jsonfile.applychanges();
     res.redirect('/');
 })
